e892cff24d7ef64ecd853b70785a16ef233daac3
ubuntu <1208266117@qq.com>
2017-04-07 10:48:18 +0800

    hp update

---
+++ neural-networks-and-deep-learning/mysrc/mynetwork3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+import cPickle
+import gzip
+
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.tensor.nnet import conv
+from theano.tensor.nnet import softmax
+from theano.tensor import shared_randomstreams
+from theano.tensor.signal import downsample
--- neural-networks-and-deep-learning/mysrc/test.py
+++ neural-networks-and-deep-learning/mysrc/test.py
@@ -1,6 +1,9 @@
-import cv2
-import numpy as np
-import gzip
-import cPickle
+import numpy 
+import theano.tensor as T
+from theano import function
+x = T.dscalar('x')
+y = T.dscalar('y')
+z = x + y
+f = function([x, y], z)
 
-a = 
+type(x)
