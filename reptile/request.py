36c78d1f338f797635a1bb97938945e808e63aa9
lxg2015 <1208266117@qq.com>
2017-06-18 21:10:57 +0800

    dell update

--- reptile/request.py
+++ reptile/request.py
@@ -8,13 +8,15 @@ def dowmloadPic(html,keyword,page):
 
 
     pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
-    i = 0
+    i = page*100
     print '找到关键词:'+keyword+'的图片，现在开始下载图片...'
     for each in pic_url:
         print '正在下载第'+str(i+1)+'张图片，图片地址:'+str(each)
         try:
             pic= requests.get(each, timeout=10)
-        except requests.exceptions.ConnectionError:
+        # except requests.exceptions.ConnectionError:
+        except Exception as e:
+
             print '【错误】当前图片无法下载'
             continue
         string = path+"pedestrian"+'_'+str(i) + '.jpg'
@@ -28,7 +30,7 @@ def dowmloadPic(html,keyword,page):
 
 if __name__ == '__main__':
     # word = raw_input("Input key word: ")
-    word = "网红"
+    word = "路人"
     for i in range(10):
         pn = str(i * 20)
         i = i + 1
