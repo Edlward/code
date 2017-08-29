cd4fb1d6bed0601a054a0b65a59794a8fda193a5
lxg2015 <1208266117@qq.com>
2017-08-29 20:28:44 +0800

    dell update

--- tinyxmltest/main.cpp
+++ tinyxmltest/main.cpp
@@ -1,4 +1,5 @@
 #include "tinyxml2.h"
+#include <string>
 
 //DOM，搞清楚类的关系
 
@@ -17,15 +18,38 @@ XMLElement *queryElemByName(XMLElement *elem, const char *name)
     return elem;
 }
 
+void write(XMLDocument *xml, std::string name, int value)
+{
+    // 插入节点
+    XMLElement *elem1 = xml->NewElement(name.c_str());
+    elem1->SetName(name.c_str());   //相当于更改节点的名字
+    elem1->SetText(value);        //给节点添加文本节点
+    xml->InsertEndChild(elem1);
+}
+
+void read(XMLDocument *xml, std::string name, int &value)
+{
+    XMLElement *elem = xml->FirstChildElement(name.c_str());
+    
+    if(elem != nullptr)
+    {
+        elem->QueryIntText(&value);
+    }
+    else
+    {
+        printf("can not find %s\n", name.c_str());
+    }
+}
+
 int main()
 {
-    XMLDocument xml;
-    char *xmlname = "test.xml";
+    XMLDocument *xml = new XMLDocument();
+    std::string xmlname = "test.xml";
 
-    if(xml.LoadFile("test.xml"))
+    if(xml->LoadFile(xmlname.c_str()))
     {
-        XMLDeclaration *decla = xml.NewDeclaration();
-        xml.InsertEndChild(decla);
+        XMLDeclaration *decla = xml->NewDeclaration();
+        xml->InsertEndChild(decla);
         printf("load fault\n");
     }
     else
@@ -33,24 +57,15 @@ int main()
         printf("load sucsessful\n");
     }
     
+    // write(xml, "fps", 30);
 
-    // 插入节点
-    XMLElement *elem1 = xml.NewElement("width");
-    elem1->SetName("height");   //相当于更改节点的名字
-    elem1->SetText(200);        //给节点添加文本节点
-    xml.InsertEndChild(elem1);
 
     // 搜寻节点
-    XMLElement *root = xml.FirstChildElement();
-    XMLElement *result = queryElemByName(root, "width");
-    if(result != nullptr)
-    {
-        printf("%s\n", result->Name());
-        
-        int tmp;
-        result->QueryIntText(&tmp);
-        printf("%d\n", tmp);
-    }
-    xml.SaveFile(xmlname);
+    int value;
+    std::string name = "fps";
+    read(xml, name, value);
+    printf("%s: %d\n", name.c_str(), value);
+
+    xml->SaveFile(xmlname.c_str());
 }
 
